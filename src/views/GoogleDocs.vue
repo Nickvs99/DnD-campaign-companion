b610600932bc49d5ae1b78471607d0e16c1a95ab
Nick van Santen <vansantennick@gmail.com>
2023-02-11 20:24:38 +0100

    Load google doc using url

--- src/views/GoogleDocs.vue
+++ src/views/GoogleDocs.vue
@@ -1,12 +1,44 @@
 <template>
-    <iframe class="google-doc" src="https://docs.google.com/document/d/e/2PACX-1vQntHl593EJQ8-uDr3-RxkAVt82--xxKyMOHRISDrqJSSVnXrB2DdhyQeDUFNOIWXv_q6LBTDX7PRUu/pub?embedded=true"></iframe>
-</template>
+    BUG for some reason their needs to be another element in this component.
+    Otherwise switching pages errors.
 
-<script></script>
+    <div id="replace">DIV TO REPLACE</div>
+
+    
+</template>
+<script>
+
+export default {
+    
+    mounted() {
+        this.loadGoogleDoc();
+    },
+    
+    methods: {
+        loadGoogleDoc() {
+
+            let d = document.createElement("div");
+            d.classList.add("google-doc");
+
+            let replace = document.getElementById("replace");
+            replace.parentElement.replaceChild(d, replace);
+
+            let url = "https://docs.google.com/document/d/e/2PACX-1vQntHl593EJQ8-uDr3-RxkAVt82--xxKyMOHRISDrqJSSVnXrB2DdhyQeDUFNOIWXv_q6LBTDX7PRUu/pub?embedded=true";
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET", url, true);
+            xhr.onload = () => {
+                d.innerHTML = xhr.responseText;
+            };
+            xhr.send();
+        },
+    },
+};
+
+</script>
 
 <style lang="scss">
     .google-doc {
-        width: 100%;
-        height: 100%;
+
     }
 </style>
