2d17e989d951ecee07da106483aa1efad356a622
Nick van Santen <vansantennick@gmail.com>
2023-02-21 21:44:32 +0100

    Load icon is now a component

--- src/components/GoogleDoc.vue
+++ src/components/GoogleDoc.vue
@@ -1,19 +1,18 @@
 <template>
-
-    <!-- BUG without the empty div the page breaks when switching pages -->
-    <div></div>
-    <div ref="divToReplace" class="load-icon-wrapper">
-        <div class="load-icon"></div>
-    </div>
-
+    
+<LoadIcon ref="loadIcon"/>
+<div ref="docWrapper" class="google-doc"></div>
     
 </template>
 
 <script>
+
+import LoadIcon from "@/components/LoadIcon.vue";
 import {sleep} from "@/util.js";
 
 export default {
     name: "GoogleDoc",
+    components: {LoadIcon},
     props: {
         "src": {
             type: String,
@@ -34,12 +33,12 @@ export default {
                 // TEMP
                 await sleep(2000);
                 
-                let divToReplace = this.$refs.divToReplace;
-
+                let docWrapper = this.$refs.docWrapper;
                 let docElement = this.parseResponse(xhr.responseText);
-                docElement.classList.add("google-doc");
 
-                divToReplace.parentElement.replaceChild(docElement, divToReplace);
+                docWrapper.appendChild(docElement);
+
+                this.$refs.loadIcon.$el.remove();
             };
             xhr.send();
         },
@@ -90,34 +89,6 @@ export default {
         padding: 5%;
         max-width: inherit;
     }
-}
-
-.load-icon-wrapper {
-    height: 200px;
-    background-color: rebeccapurple;
-
-
-    display:flex;
-    align-items: center;
-    justify-content: center;
-
-}
-
-@keyframes load-animation {
-    0% {transform: rotate(0deg) scale(1);}
-    50% {transform: rotate(180deg) scale(1.5);}
-    100% {transform: rotate(360deg) scale(1);}    
-}
-
-.load-icon {
-    border:solid;
-
-    width: 25px;
-    height: 25px;
-
-    animation-name: load-animation;
-    animation-duration: 1s;
-    animation-iteration-count: infinite;
 }
 
 </style>
---
+++ src/components/LoadIcon.vue
@@ -0,0 +1,42 @@
+<template>
+
+<div ref="divToReplace" class="load-icon-wrapper">
+    <div class="load-icon"></div>
+</div>
+
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+
+.load-icon-wrapper {
+    height: 200px;
+    background-color: rebeccapurple;
+
+    display:flex;
+    align-items: center;
+    justify-content: center;
+}
+
+@keyframes load-animation {
+    0% {transform: rotate(0deg) scale(1);}
+    50% {transform: rotate(180deg) scale(1.5);}
+    100% {transform: rotate(360deg) scale(1);}    
+}
+
+.load-icon {
+    border:solid;
+
+    width: 25px;
+    height: 25px;
+
+    animation-name: load-animation;
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+}
+
+</style>
