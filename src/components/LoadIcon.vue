63206c1f5746d5610cce12cbcb9ac1b85be3e813
Nick van Santen <vansantennick@gmail.com>
2023-02-22 21:06:41 +0100

    Show text under load icon

--- src/components/LoadIcon.vue
+++ src/components/LoadIcon.vue
@@ -2,12 +2,44 @@
 
 <div ref="divToReplace" class="load-icon-wrapper">
     <div class="load-icon"></div>
+    <div class="load-message" ref="loadMessage" data-trailing-dots=""> {{ message }}</div>
 </div>
 
 </template>
 
 <script>
 
+export default {
+    props: {
+        "message": {
+            type: String,
+            required: false,
+            default: "",
+        }
+    },
+
+    data() {
+        return {
+            nDots: 0,
+        };
+    },
+
+    mounted() {
+        this.updateInterval = window.setInterval(() => {
+            this.nDots = (this.nDots + 1) % 4;
+
+            if(this.message != ""){
+                this.$refs.loadMessage.setAttribute("data-trailing-dots", ".".repeat(this.nDots));
+            }
+            else {
+                this.$refs.loadMessage.setAttribute("data-trailing-dots", "");
+            }
+        }, 500);
+    },
+    unmounted() {
+        window.clearInterval(this.updateInterval);
+    }
+};
 
 </script>
 
@@ -18,6 +50,8 @@
     background-color: rebeccapurple;
 
     display:flex;
+    flex-direction: column;
+    row-gap: 1.4rem;
     align-items: center;
     justify-content: center;
 }
@@ -37,6 +71,18 @@
     animation-name: load-animation;
     animation-duration: 1s;
     animation-iteration-count: infinite;
+}
+
+.load-message {
+    color: white;
+    font-size: 1.5rem;
+}
+
+.load-message::after {
+    content: attr(data-trailing-dots);
+    
+    /* This makes sure that the message is not constantly jumping around */
+    position:absolute;
 }
 
 </style>
