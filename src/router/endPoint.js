45c1988d09d93d252b26ec202cc755379c6b5487
Nick van Santen <vansantennick@gmail.com>
2023-02-25 17:32:38 +0100

    Props are now correctly parsed again.

--- src/router/endPoint.js
+++ src/router/endPoint.js
@@ -9,6 +9,8 @@ export class Endpoint {
     
     constructor(componentPath, props = {}) {
 
+        this.componentName = this.getComponentName(componentPath);
+
         this.vueComponent = () => import(`@/${componentPath}`);
         this.props = props;
 
@@ -22,12 +24,22 @@ export class Endpoint {
      * and then using instanceof, but that requires additional imports.
      */
     parseProps() {
-        // TODO this does not work yet
-        if(this.vueComponent.name === "ExpandableImage") {
+
+        if(this.componentName === "ExpandableImage") {
             this.props["src"] = require(`@/assets/images/${this.props["src"]}`);
         }
-        else if(this.vueComponent.name === "ImageDoc") {
+        else if(this.componentName === "ImageDoc") {
             this.props["imgSrc"] = require(`@/assets/images/${this.props["imgSrc"]}`);
         }
+    }
+
+    getComponentName(componentPath) {
+
+        // Get filename of path
+        let pathSplit = componentPath.split("/");
+        let last = pathSplit[pathSplit.length - 1];
+
+        // remove extension
+        return last.split(".").slice(0, -1).join(".");
     }
 }
