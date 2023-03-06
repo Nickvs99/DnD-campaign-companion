519d0a12917ad9386abfb7c25357428824cc8eb5
Nick van Santen <vansantennick@gmail.com>
2023-03-06 20:09:41 +0100

    Obtain name, class, race, level, and stats from json file

---
+++ src/components/StatBlock.vue
@@ -0,0 +1,49 @@
+<template>
+
+<div class="stat-block">
+    <div class="stat-block-name">{{ name }}</div>
+    <div class="stat-block-value">{{ value }}</div>
+</div>
+
+</template>
+
+<script>
+
+export default {
+    name: "StatBlock",
+    props: {
+        "name": {
+            required: true,
+            type: String,
+        },
+        "value": {
+            required: true,
+            type: Number,
+        }
+    }
+};
+
+</script>
+
+<style lang="scss">
+
+.stat-block {
+    aspect-ratio: 1/1;
+    width: 40px;
+
+    background-color: darkblue;
+}
+
+.stat-block-name {
+    margin: 1em;
+
+    background-color: darkcyan;
+}
+
+.stat-block-value {
+    margin: 1em;
+
+    background-color: chocolate;
+}
+
+</style>
--- src/views/CharacterView.vue
+++ src/views/CharacterView.vue
@@ -2,18 +2,25 @@
 
 <div class="character-page-container">
 
-    {{ character_id }}
+    <h1>{{ name }}</h1>
+    <h2>{{ race }}</h2>
+    <h2>{{ className }} - {{ subclass }}</h2>
+    <h2>Level: {{ level }}</h2>
 
+    <div class="stat-block-container" v-if="stats[0].value != null">
+        <StatBlock v-for="stat in stats" :key="stat.name" :name="stat.name" :value="stat.value"/>
+    </div>
 </div>
 
 </template>
 
 <script>
-
+import StatBlock from "@/components/StatBlock.vue";
 import data from "@/assets/character.json";
 
 export default {
     name: "CharacterView",
+    components: { StatBlock },
     props: {
         "character_id": {
             required: true,
@@ -23,6 +30,19 @@ export default {
     data() {
         return {
             data: data,
+            name: null,
+            race: null,
+            className: null,
+            subclass: null,
+            level: null,
+            stats: [
+                {"name": "Strength", "value": null},
+                {"name": "Dexterity", "value": null},
+                {"name": "Constitution", "value": null},
+                {"name": "Intelligence", "value": null},
+                {"name": "Wisdom", "value": null},
+                {"name": "Charisma", "value": null},
+            ]
         };
     },
     mounted() {
@@ -30,8 +50,62 @@ export default {
     },
     methods: {
         parseJsonResponse(json) {
-            console.log(json["data"]["id"]);
+            this.setClass(json);
+            this.setLevel(json);
+            this.setName(json);
+            this.setRace(json);
+            this.setStats(json);
+        },
+        setClass(json) {
 
+            let classes = json.data.classes;
+            if (classes.length !== 1) {
+                alert("This page does not work yet with Multiclassing. Please notify Nick :) ");
+            }
+
+            this.className = classes[0].definition.name;
+            this.subclass = classes[0].subclassDefinition.name;
+        },
+        setLevel(json) {
+
+            let level = 0;
+            for(let clss of json.data.classes) {
+                level += clss.level;
+            }
+            this.level = level;
+        },
+        setName(json) {
+            this.name = json.data.name;
+        },
+        setRace(json) {
+            this.race = json.data.race.fullName;
+        },
+        setStats(json) {
+            let baseStats = json.data.stats;
+            let bonusStats = json.data.bonusStats;
+
+            for(let i = 0; i < baseStats.length; i ++) {
+                this.stats[i].value = baseStats[i].value + bonusStats[i].value;
+            }
+
+            // Racial bonus and ability score improvment
+            let raceModifiers = json.data.modifiers.race;
+            let classModifiers = json.data.modifiers.class;
+
+            let modifiers = raceModifiers.concat(classModifiers);
+            for(let modifier of modifiers) {
+                
+                // Stat modifiers are of type bonus and the subtype is written like 'statName'-score
+                // e.g. strength-score
+                if(modifier.type !== "bonus" || !modifier.subType.endsWith("-score")) {
+                    continue;
+                }
+
+                let statName = modifier.subType.split("-")[0];
+                let statIndex = this.stats.findIndex(stat => stat.name.toLowerCase() === statName);
+                
+                this.stats[statIndex].value += modifier.value;
+            }
         }
     }
 };
