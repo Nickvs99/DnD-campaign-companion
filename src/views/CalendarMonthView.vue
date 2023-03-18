64e955f27bcbadeec417fe3dd24fbbf9f979250f
Nick van Santen <vansantennick@gmail.com>
2023-03-18 19:28:17 +0100

    Add daynames and month name to calendar

--- src/views/CalendarMonthView.vue
+++ src/views/CalendarMonthView.vue
@@ -1,15 +1,22 @@
 <template>
 
-<div class="calendar-container">
+<div class="calendar-view-container">
     <div class="calendar-title">{{ month }} - {{ year }}</div>
-    <div ref="monthContainer" class="month-container">
-        <div v-for="day in nDays" class="day-container" :key="day">
-            
-            <div class="day-count">{{ day }}</div>
-            <div class="event-container">
-                <CalendarEvent v-for="evt in events[day]" :event="evt" :key="evt" />
+    <div class="calendar-container">
+
+        <div class="day-name-container">
+            <div v-for="dayName in dayNames" :key="dayName" class="day-name calendar-item">{{dayName.slice(0,3) }}</div>
+        </div>
+
+        <div ref="monthContainer" class="month-container">
+            <div v-for="day in nDays" class="day-container calendar-item" :key="day">
+                <div class="day-count">{{ day }}</div>
+                <div class="event-container">
+                    <CalendarEvent v-for="evt in events[day]" :event="evt" :key="evt" />
+                </div>
             </div>
         </div>
+
     </div>
 </div>
 
@@ -35,6 +42,7 @@ export default {
             events: null,
             nDays: null,
             nWeeks: null,
+            dayNames: null,
         };
     },
     mounted() {
@@ -42,6 +50,7 @@ export default {
         this.events = this.calendar.events[this.year][this.month];
         this.nWeeks = this.calendar.weeksPerMonth;
         this.nDays = this.nWeeks * this.calendar.dayNames.length;
+        this.dayNames = this.calendar.dayNames;
 
         window.addEventListener("resize", this.setCalendarHeight);
         this.setCalendarHeight();
@@ -67,13 +76,39 @@ export default {
 
 @import "@/styles/_mixins.scss";
 
-.content-wrapper:has(.calendar-container) {
+.content-wrapper:has(.calendar-view-container) {
     // Show overflow since the month container should span the entire width of the screen
-    overflow:visible;
+    overflow: visible;
+
+    // This still creates a new Block formatting context
+    float: left;
 }
 
 .calendar-title {
-    font-size: 3rem;
+    text-align: center;
+    font-size: 1.5rem;
+    margin: 0.5em 0;
+}
+
+.calendar-container {
+    // Fill entire width, since each px is valuable
+    margin: 0 -5%;
+}
+
+.calendar-item {
+    overflow: hidden;
+}
+
+.day-name-container {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-template-rows: calc(1rem + 4px);
+    margin: 2px 0;
+}
+
+.day-name {
+    text-align: center;
+    font-style: italic;
 }
 
 .month-container {
@@ -89,9 +124,6 @@ export default {
     // Removes double borders
     border-top: 1px solid rgba(0, 0, 0, 0.15);
     border-left: 1px solid rgba(0, 0, 0, 0.15);
-
-    // Fill entire width, since each px is valuable
-    margin: 0 -5%;
 }
 
 .day-container {    
@@ -103,8 +135,6 @@ export default {
 
     border-bottom: 1px solid rgba(0, 0, 0, 0.15);
     border-right: 1px solid rgba(0, 0, 0, 0.15);
-
-    overflow: hidden;
 }
 
 .day-count {    
