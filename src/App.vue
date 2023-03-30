5bc67fb73371c32239b9bbde635ab16485f7a2bd
Nick van Santen <vansantennick@gmail.com>
2023-03-30 21:20:06 +0200

    Added alert message for large screens

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@
 <NavBar />
 
 <router-view v-slot="{Component}">
+    <AlertMessage v-if="screenWidth > 1000">
+        This site is intended for mobile devices. No resources have been expended to create a visually pleasing layout for larger screens. We are sorry for this abombination.
+    </AlertMessage>
+    
     <div id="content-wrapper" ref="contentWrapper">
         <transition name="fade" mode="out-in">
             <CenterToScreen v-if="isLoading">
@@ -18,29 +22,41 @@
 
 <script>
 
+import AlertMessage from "@/components/AlertMessage.vue";
 import BackGround from "@/components/BackGround.vue";
-import CenterToScreen from "./components/CenterToScreen.vue";
-import LoadIcon from "./components/LoadIcon.vue";
+import CenterToScreen from "@/components/CenterToScreen.vue";
+import LoadIcon from "@/components/LoadIcon.vue";
 import NavBar from "@/components/NavBar.vue";
 
 export default {
     name: "App",
-    components: {BackGround, CenterToScreen, LoadIcon, NavBar},
+    components: { AlertMessage, BackGround, CenterToScreen, LoadIcon, NavBar},
     data() {
         return {
             // This value is changed by the router
             isLoading: false,
+
+            screenWidth: null,
         };
     },
     mounted(){
-        window.addEventListener("resize", this.setMinHeight);
-        this.setMinHeight();
+        window.addEventListener("resize", this.onResize);
+        this.onResize();
     },
     unmounted() {
-        window.removeEventListener("resize", this.setMinHeight);
+        window.removeEventListener("resize", this.onResize);
     },
     methods: {
         
+        onResize() {
+            this.setMinHeight();
+            this.setScreenWidth();
+        },
+
+        setScreenWidth() {
+            this.screenWidth = window.innerWidth;
+        },
+
         /**
          * Sets mimimum height such that it fills remaining height to the bottom
          */
---
+++ src/components/AlertMessage.vue
@@ -0,0 +1,43 @@
+<template>
+
+<div class="alert-message-container">
+    <slot/>
+    <CloseIcon @click="destroyElement"/>
+</div>
+
+</template>
+
+<script>
+
+import CloseIcon from "@/assets/icons/CloseIcon.vue";
+
+export default {
+    name: "AlertMessage",
+    components: { CloseIcon },
+    methods: {
+        destroyElement() {
+            this.$el.remove();
+        }
+    }
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+.alert-message-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    
+    padding: 0.5em;
+    column-gap: 0.5em;
+    
+    background-color: var(--secondary-color);
+}
+
+svg {
+    width: 1.25em;
+}
+
+</style>
